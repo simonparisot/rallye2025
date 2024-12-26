ce9d784589e0e864276aee1e4e10ff3d31a2832a
Simon <parisot.simon@gmail.com>
2024-12-26 09:58:04 -0400

    modification of backend

--- index.js
+++ index.js
@@ -1,17 +1,10 @@
 const https = require('https');
 
 exports.handler = async (event) => {
-    // Log the received event
-    console.log('Received event:', event);
-
     // Get the term from query parameters
     const term = event.queryStringParameters && event.queryStringParameters.term;
 
-    console.log('Testing:', term);
-
     const url = `https://tinyurl.com/RH2025-${term}`;
-
-    console.log('Testing:', url);
 
     return new Promise((resolve, reject) => {
         https.get(url, (res) => {
